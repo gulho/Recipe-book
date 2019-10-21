16aa9bdc6d0b8a187efe3d9e5028de06fa3da699
gulho <gulhos@gmail.com>
2019-10-22 00:01:05 +0300

    stage 2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recepies: Recipe[] = [
+    new Recipe('A test Recipe', 'Recipe descrition', 'http://www.fnstatic.co.uk/images/content/recipe/korean-bbq-chicken.jpeg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingridient} from '../recipes/shared/ingridients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingridients: Ingridient[] = [
+    new Ingridient('apple', 5),
+    new Ingridient('Banan', 10)
+  ];
 
   constructor() { }
 
