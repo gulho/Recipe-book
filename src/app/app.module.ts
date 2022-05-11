87b898909aa5a7a6d3db6acbcac2a9a5028c6b73
kirill lyappe <kirill.lyappe@avalanchelabs.com>
2022-05-11 21:42:40 +0300

    Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {DropdownDirective} from "./shared/dropdowsn.derective";
-import {ShoppingListService} from "./shopping-list/shoping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.service";
 import {ReactiveFormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./auth/auth.component";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {AlertComponent} from "./shared/alert/alert.component";
-import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
@@ -30,13 +25,9 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
