4a871e586379f9744bbcaa8d7476f12dc4280596
kirill lyappe <kirill.lyappe@avalanchelabs.com>
2022-05-16 17:00:06 +0300

    Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: "https://ng-course-recipe-book-61984-default-rtdb.europe-west1.firebasedatabase.app/",
+  firebaseApiKey: 'AIzaSyCmhRhH9L3mvDuD7CW3hlgelOdAmIN7i4Y',
+  firebaseUrl: 'https://identitytoolkit.googleapis.com/v1/'
 };
