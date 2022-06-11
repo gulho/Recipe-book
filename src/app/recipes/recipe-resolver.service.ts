cbd3562ac2d9b309a3f896b12bcc28c38bdbcddb
kirill lyappe <kirill.lyappe@avalanchelabs.com>
2022-06-11 19:30:46 +0300

    Add store for Recipes stage I

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -3,14 +3,13 @@ import {Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Observable} from "rxjs";
 import {Store} from "@ngrx/store";
-import {Actions} from "@ngrx/effects";
 
 import * as fromApp from "../store/app.reducer";
 import * as RecipeActions from "../recipes/store/recipe.action";
 
 @Injectable({providedIn: 'root'})
 export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(private store: Store<fromApp.AppState>, private actions$: Actions) {
+  constructor(private store: Store<fromApp.AppState>) {
   }
 
   resolve(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
